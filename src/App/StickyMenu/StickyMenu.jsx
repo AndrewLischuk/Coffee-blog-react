92e487a9f70c853902350cccd56a3dd5fc9e1108
MarshallGunn <marshallgunn59@gmail.com>
2021-04-29 21:38:11 +0300

    checkpoint

--- src/App/StickyMenu/StickyMenu.jsx
+++ src/App/StickyMenu/StickyMenu.jsx
@@ -4,6 +4,7 @@ import Menu from '../Header/Menu/Menu'
 import Logo from '../Header/Logo/Logo'
 
 const StickyMenu = () => {
+    
     return (
         <>
             <div className="sticky-menu deactivate-el">
@@ -15,7 +16,7 @@ const StickyMenu = () => {
                         <div className="col-xs-3 col-sm-3 col-md-8 col-lg-8 col-xl-8">
                             <Menu/>
                             <div className="col-xs-3 col-sm-3 nav-mobile-menu-none">
-                                <div className="nav-mobile-menu">
+                                <div className="nav-mobile-menu" onClick={() => console.log("clicked")}>
                                     <span></span>
                                     <span></span>
                                     <span></span>
